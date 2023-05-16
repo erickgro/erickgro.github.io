be7c1f1fa679235ac4ee661201d3bb4610c912fc
erickgro <69096915+erickgro@users.noreply.github.com>
2023-05-16 03:03:04 -0600

    Copy buttons fixed

--- script.js
+++ script.js
@@ -1,254 +1,225 @@
-// Initialize variables and get UI elements
-let mediaStream;
-let VoiceRecognized = false;
-let timer = 0;
-let globalTimer = 0;
-let timeoutId = null;
-let intervalId = null;
-let globalIntervalId = null;
-let stopRequested = false;
-let isListening = false;
-let voiceRecognitionStart = 0;  // Variable to hold the start time of a voice recognition fragment
-const voiceStatus = document.getElementById('voice-status');
-const timerDisplay = document.getElementById('timer');
-const transcriptList = document.getElementById('transcript-list');
-const globalTimerDisplay = document.getElementById('global-timer');
+document.addEventListener('DOMContentLoaded', (event) => {
 
-let recognition;
-let mediaRecorder;
-let recordedChunks = [];
-let audioElement = null;  // Initialize audioElement in global scope
-let transcripts = []; // Add this line to store the transcripts and timestamps
+    // Initialize variables and get UI elements
+    let mediaStream;
+    let VoiceRecognized = false;
+    let timer = 0;
+    let globalTimer = 0;
+    let timeoutId = null;
+    let intervalId = null;
+    let globalIntervalId = null;
+    let stopRequested = false;
+    let isListening = false;
+    let voiceRecognitionStart = 0;
+    const voiceStatus = document.getElementById('voice-status');
+    const timerDisplay = document.getElementById('timer');
+    const transcriptList = document.getElementById('transcript-list');
+    const globalTimerDisplay = document.getElementById('global-timer');
 
-// Set up SpeechRecognition
-function setupSpeechRecognition() {
-    recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
-    recognition.lang = 'es-MX'; // Set language to Spanish (Mexico)
+    let recognition;
+    let mediaRecorder;
+    let recordedChunks = [];
+    let audioElement = null;
+    let transcripts = [];
 
-    // Define SpeechRecognition event handlers
-    recognition.onstart = function() {
-        isListening = true; // Set isListening to true when the service starts
-    }
+    function setupSpeechRecognition() {
+        recognition = new webkitSpeechRecognition();
+        recognition.continuous = true;
+        recognition.interimResults = true;
+        recognition.lang = 'es-MX';
 
-    recognition.onresult = function(event) {
-        for (let i = event.resultIndex; i < event.results.length; i++) {
-            if (VoiceRecognized === false) {
-                voiceRecognitionStart = globalTimer;
-                VoiceRecognized = true;
+        recognition.onstart = function() {
+            isListening = true;
+        }
+
+        recognition.onresult = function(event) {
+            for (let i = event.resultIndex; i < event.results.length; i++) {
+                if (VoiceRecognized === false) {
+                    voiceRecognitionStart = globalTimer;
+                    VoiceRecognized = true;
+                }
+
+                if (event.results[i].isFinal) {
+                    let transcript = event.results[i][0].transcript;
+
+                    let timestamp = new Date(voiceRecognitionStart * 1000).toISOString().substr(14, 5);
+
+                    transcripts.push({timestamp: timestamp, transcript: transcript});
+
+                    let listItem = document.createElement('li');
+                    let link = document.createElement('a');
+                    link.href = '#';
+                    link.textContent = timestamp;
+                    link.onclick = function() {
+                        let [minutes, seconds] = timestamp.split(':');
+                        audioElement.currentTime = minutes * 60 + Number(seconds);
+                        audioElement.play();
+                        return false; 
+                    };
+                    listItem.appendChild(link);
+                    listItem.appendChild(document.createTextNode(` - ${transcript}`));
+
+                    let copyButton = document.createElement('button');
+                    copyButton.textContent = 'Copy';
+                    copyButton.className = 'copy-button';
+                    copyButton.addEventListener('click', function() {
+                        let textarea = document.createElement('textarea');
+                        textarea.textContent = `${timestamp} - ${transcript}`;
+                        document.body.appendChild(textarea);
+                        textarea.select();
+                        document.execCommand('copy');
+                        document.body.removeChild(textarea);
+                    });
+
+                    listItem.appendChild(copyButton);
+                    transcriptList.appendChild(listItem);
+                }
             }
 
-            // Only add final results to the list
-            if (event.results[i].isFinal) {
-                // Get transcript
-                let transcript = event.results[i][0].transcript;
+            voiceStatus.textContent = 'Voice detected';
+            voiceStatus.style.color = 'green';
 
-                // Add timestamp to the transcript
-                let timestamp = new Date(voiceRecognitionStart * 1000).toISOString().substr(14, 5);
-
-                // Save the transcript and timestamp to the array
-                transcripts.push({timestamp: timestamp, transcript: transcript});
-
-                // Add transcript to the list
-                let listItem = document.createElement('li');
-                listItem.textContent = `${timestamp} - ${transcript}`;
-                transcriptList.appendChild(listItem);
-
-                // Create "Copy" button
-                let copyButton = document.createElement('button');
-                copyButton.textContent = 'Copy';
-                copyButton.className = 'copy-button';
-                copyButton.addEventListener('click', function() {
-                    // Create a textarea to hold the text to copy
-                    let textarea = document.createElement('textarea');
-                    textarea.textContent = `${timestamp} - ${transcript}`;
-                    document.body.appendChild(textarea);
-                    textarea.select();
-                    document.execCommand('copy');
-                    document.body.removeChild(textarea);
-                });
-
-                // Add "Copy" button to the list item
-                listItem.appendChild(copyButton);
-            
-            }
-        }
-
-        // Set VoiceRecognized to true when human voice is detected
-        voiceStatus.textContent = 'Voice detected';
-        voiceStatus.style.color = 'green';
-
-        // Reset timer
-        timer = 0;
-        timerDisplay.textContent = timer;
-
-        // Set VoiceRecognized to false after 2 seconds of not hearing a human voice
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(function() {
-            VoiceRecognized = false;
-            voiceStatus.textContent = 'Undetected';
-            voiceStatus.style.color = 'red';
-            startTimer();
-        }, 2000);
-
-        if (transcripts.length === 1) {
-            // Enable the "Copy all" button when the first transcription is added
-            document.getElementById('copy-all-button').disabled = false;
-        }
-        // Set up the "Copy all" button
-        document.getElementById('copy-all-button').addEventListener('click', function() {
-            let allTranscriptions = transcripts.map(t => `${t.timestamp} - ${t.transcript}`).join('\n');
-            navigator.clipboard.writeText(allTranscriptions)
-                .then(function() {
-                    /* clipboard successfully set */
-                    console.log('Clipboard successfully set');
-                }, function() {
-                    /* clipboard write failed */
-                    console.log('Clipboard write failed');
-                });
-        });
-    }
-
-    // Restart the service when it ends
-    recognition.onend = function() {
-        isListening = false; // Set isListening to false when the service ends
-        // Check if the stopListening function was called, if not restart the service
-        if(!stopRequested && !isListening) { // Check if the service is not already running
-            recognition.start();
-        }
-    }
-
-    recognition.onerror = function(event) {
-        console.log('Error occurred in recognition: ' + event.error);
-        if (event.error === 'no-speech') {
-            recognition.start();
-        }
-    };
-}
-
-// Function to start the timer
-function startTimer() {
-    // Clear existing interval
-    if (intervalId) {
-        clearInterval(intervalId);
-    }
-    timer = 0;
-    intervalId = setInterval(function() {
-        if (!VoiceRecognized) {
-            timer++;
+            timer = 0;
             timerDisplay.textContent = timer;
+
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(function() {
+                VoiceRecognized = false;
+                voiceStatus.textContent = 'Undetected';
+                voiceStatus.style.color = 'red';
+                startTimer();
+            }, 2000);
+
+            if (transcripts.length === 1) {
+                // Enable the "Copy all" button when the first transcription is added
+                let copyAllButton = document.getElementById('copy-all');
+                copyAllButton.disabled = false;
+
+                // Set up the "Copy all" button
+                copyAllButton.addEventListener('click', function() {
+                    let allTranscriptions = transcripts.map(t => `${t.timestamp} - ${t.transcript}`).join('\n');
+                    navigator.clipboard.writeText(allTranscriptions)
+                        .then(function() {
+                            /* clipboard successfully set */
+                            console.log('Clipboard successfully set');
+                        }, function() {
+                            /* clipboard write failed */
+                            console.log('Clipboard write failed');
+                        });
+                });
+            }
         }
-    }, 1000);
-}
 
-// Function to start the global timer
-function startGlobalTimer() {
-    globalTimer = 0;
-    globalIntervalId = setInterval(function() {
-        globalTimer++;
-        // Convert the timer value to mm:ss format and display it in the UI
-        let minutes = Math.floor(globalTimer / 60);
-        let seconds = globalTimer % 60;
-        globalTimerDisplay.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-    }, 1000);
-}
+        recognition.onend = function() {
+            isListening = false;
+            if(!stopRequested && !isListening) {
+                recognition.start();
+            }
+        }
 
-// Function to stop the global timer
-function stopGlobalTimer() {
-    clearInterval(globalIntervalId);
-    // Reset the global timer display in the UI
-    globalTimerDisplay.textContent = '0';
-}
-
-// Function to start listening to the microphone
-function startListening() {
-    // Reset stopRequested flag
-    stopRequested = false;
-    setupSpeechRecognition();
-    if(!isListening) { // Check if the service is not already running
-        recognition.start();
-    }
-    startTimer();
-    startGlobalTimer();  // Start the global timer
-
-    // Start recording
-    navigator.mediaDevices.getUserMedia({ audio: true })
-        .then(function(stream) {
-            mediaStream = stream;  // Save the stream to the global variable
-            mediaRecorder = new MediaRecorder(stream);
-            mediaRecorder.start();
-
-            mediaRecorder.ondataavailable = function(e) {
-                recordedChunks.push(e.data);
-            };
-        });
-}
-
-// Function to stop listening to the microphone
-function stopListening() {
-    // Set stopRequested flag
-    stopRequested = true;
-    recognition.stop();
-    clearTimeout(timeoutId);
-    clearInterval(intervalId); // Clear the timer interval
-    stopGlobalTimer();  // Stop the global timer
-
-    // Stop accessing the microphone
-    if (mediaRecorder.stream) {
-        let tracks = mediaRecorder.stream.getTracks();
-        tracks.forEach(track => track.stop());
+        recognition.onerror = function(event) {
+            console.log('Error occurred in recognition: ' + event.error);
+            if (event.error === 'no-speech') {
+                recognition.start();
+            }
+        };
     }
 
-    // Stop recording
-    mediaRecorder.stop();
+    function startTimer() {
+        if (intervalId) {
+            clearInterval(intervalId);
+        }
+        timer = 0;
+        intervalId = setInterval(function() {
+            if (!VoiceRecognized) {
+                timer++;
+                timerDisplay.textContent = timer;
+            }
+        }, 1000);
+    }
 
-    // Process the recorded audio data into an ogg file with opus codec
-    mediaRecorder.onstop = function(e) {
-        const blob = new Blob(recordedChunks, {
-            'type' : 'audio/ogg; codecs=opus'
-        });
-        recordedChunks = [];
-        let audioURL = window.URL.createObjectURL(blob);
+    function startGlobalTimer() {
+        globalTimer = 0;
+        globalIntervalId = setInterval(function() {
+            globalTimer++;
+            let minutes = Math.floor(globalTimer / 60);
+            let seconds = globalTimer % 60;
+            globalTimerDisplay.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        }, 1000);
+    }
 
-        // Create a new audio element if one doesn't exist yet
-        if (!audioElement) {
-            audioElement = new Audio();
-            audioElement.controls = true;
+    function stopGlobalTimer() {
+        clearInterval(globalIntervalId);
+        globalTimerDisplay.textContent = '0';
+    }
+
+    function startListening() {
+        stopRequested = false;
+        setupSpeechRecognition();
+        if(!isListening) {
+            recognition.start();
+        }
+        startTimer();
+        startGlobalTimer();
+
+        navigator.mediaDevices.getUserMedia({ audio: true })
+            .then(function(stream) {
+                mediaStream = stream;
+                mediaRecorder = new MediaRecorder(stream);
+                mediaRecorder.start();
+
+                mediaRecorder.ondataavailable = function(e) {
+                    recordedChunks.push(e.data);
+                };
+            });
+    }
+
+    function stopListening() {
+        stopRequested = true;
+        recognition.stop();
+        clearTimeout(timeoutId);
+        clearInterval(intervalId);
+        stopGlobalTimer();
+
+        if (mediaRecorder.stream) {
+            let tracks = mediaRecorder.stream.getTracks();
+            tracks.forEach(track => track.stop());
         }
 
-        // Update the source of the audio element
-        audioElement.src = audioURL;
+        mediaRecorder.stop();
 
-        // Add the audio element to the UI
-        let audioControlBar = document.getElementById('audio-control-bar');
-        audioControlBar.appendChild(audioElement);
+        mediaRecorder.onstop = function(e) {
+            const blob = new Blob(recordedChunks, {
+                'type' : 'audio/ogg; codecs=opus'
+            });
+            recordedChunks = [];
+            let audioURL = window.URL.createObjectURL(blob);
 
-        // Add hyperlinks to the timestamps
-        for (let i = 0; i < transcriptList.children.length - 1; i++) { // -1 to exclude the audio player item
-            let listItem = transcriptList.children[i];
-            let timestamp = transcripts[i].timestamp;
-            let link = document.createElement('a');
-            link.href = '#';
-            link.textContent = timestamp;
-            link.onclick = function() {
-                // Convert timestamp to seconds and set as current time for audio element
-                let [minutes, seconds] = timestamp.split(':');
-                audioElement.currentTime = minutes * 60 + Number(seconds);
-                audioElement.play();
-                return false; // Prevent the default action
-            };
-            listItem.innerHTML = ''; // Clear the list item content
-            listItem.appendChild(link);
-            listItem.appendChild(document.createTextNode(` - ${transcripts[i].transcript}`)); // Append the transcript to the list item
-        }
-    };
-}
+            if (!audioElement) {
+                audioElement = new Audio();
+                audioElement.controls = true;
+            }
 
-// Get the Start and Stop buttons
-const startButton = document.getElementById('start-button');
-const stopButton = document.getElementById('stop-button');
+            audioElement.src = audioURL;
 
-// Set up button event listeners
-startButton.addEventListener('click', startListening);
-stopButton.addEventListener('click', stopListening);
+            let audioControlBar = document.getElementById('audio-control-bar');
+            audioControlBar.appendChild(audioElement);
+        };
+    }
+
+    const startButton = document.getElementById('start-button');
+    const stopButton = document.getElementById('stop-button');
+
+    startButton.addEventListener('click', startListening);
+    stopButton.addEventListener('click', stopListening);
+
+    document.getElementById('copy-all').addEventListener('click', function() {
+        let allTranscriptions = transcripts.map(t => `${t.timestamp} - ${t.transcript}`).join('\n');
+        navigator.clipboard.writeText(allTranscriptions)
+            .then(function() {
+                console.log('Clipboard successfully set');
+            }, function() {
+                console.log('Clipboard write failed');
+            });
+    });
+});
