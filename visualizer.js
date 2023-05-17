025ce70cf6615702479ca9f25d654c6619030093
erickgro <69096915+erickgro@users.noreply.github.com>
2023-05-16 21:21:37 -0600

    Step back
    Delete apple css, bing back circle echo

--- visualizer.js
+++ visualizer.js
@@ -32,8 +32,10 @@ function visualize() {
     visualizerAnimationId = requestAnimationFrame(visualize);
     analyser.getByteFrequencyData(dataArray);
 
-    // Clear the canvas
-    visualizerContext.clearRect(0, 0, visualizerCanvas.width, visualizerCanvas.height);
+    // Overlay a semi-transparent rectangle to gradually fade out the older drawings
+    visualizerContext.fillStyle = 'rgba(255, 255, 255, 0.1)';  // Adjust the alpha value (0.1) as needed
+    visualizerContext.fillRect(0, 0, visualizerCanvas.width, visualizerCanvas.height);
+    visualizerContext.fillStyle = 'black';
 
     // Calculate the center and radius of the circle
     let centerX = visualizerCanvas.width / 2;
