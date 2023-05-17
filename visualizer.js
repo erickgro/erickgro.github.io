b7e8ed60f2f5a0fc5646ac41de43ee986685a0af
erickgro <69096915+erickgro@users.noreply.github.com>
2023-05-16 19:49:27 -0600

    Visual 3

--- visualizer.js
+++ visualizer.js
@@ -14,6 +14,8 @@ dataArray = new Uint8Array(bufferLength);
 visualizerCanvas = document.createElement('canvas');
 visualizerCanvas.width = 500;
 visualizerCanvas.height = 500;
+visualizerCanvas.style.border = '1px solid red';
+
 document.body.appendChild(visualizerCanvas);
 visualizerCanvas.style.display = 'none';
 
@@ -29,7 +31,18 @@ function startVisualizer(stream) {
 function visualize() {
     visualizerAnimationId = requestAnimationFrame(visualize);
     analyser.getByteFrequencyData(dataArray);
-    // ... (rest of the visualize function)
+    
+    // Clear the canvas
+    visualizerContext.clearRect(0, 0, visualizerCanvas.width, visualizerCanvas.height);
+    
+    // Draw the frequency data
+    for(let i = 0; i < bufferLength; i++) {
+        // Calculate the height of the bar for this frequency
+        let barHeight = dataArray[i] / 2;  // Divide by 2 to fit the canvas height
+        
+        // Draw the bar
+        visualizerContext.fillRect(i * 4, visualizerCanvas.height - barHeight, 2, barHeight);
+    }
 }
 
 function stopVisualizer() {
