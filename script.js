247c5b065d2507a0f91976cc5a2712b16e286b84
erickgro <69096915+erickgro@users.noreply.github.com>
2023-05-22 02:43:18 -0600

    Styled

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// import { startVisualizer, stopVisualizer } from './visualizer.js';
+/// import { startVisualizer, stopVisualizer } from './visualizer.js';
 
 document.addEventListener('DOMContentLoaded', (event) => {
-
     // Initialize variables and get UI elements    
     let mediaStream;
     let VoiceRecognized = false;
@@ -17,6 +16,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const timerDisplay = document.getElementById('timer');
     const transcriptList = document.getElementById('transcript-list');
     const globalTimerDisplay = document.getElementById('global-timer');
+    const controlBar = document.getElementById('control-bar');
+    let link = document.createElement('a');
+    link.href = '#';
+    link.classList.add('link-disabled');  // Add the 'link-disabled' class initially
+
 
     let recognition;
     let mediaRecorder;
@@ -43,26 +47,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
                 if (event.results[i].isFinal) {
                     let transcript = event.results[i][0].transcript;
-
-                    // Subtract 2 seconds from the voiceRecognitionStart time before creating the timestamp
-                    let timestamp = new Date((voiceRecognitionStart - 1) * 1000).toISOString().substr(14, 5);
+                    let timestamp = new Date((voiceRecognitionStart - 2) * 1000).toISOString().substr(14, 5);
 
                     transcripts.push({timestamp: timestamp, transcript: transcript});
 
                     let listItem = document.createElement('li');
-
                     let link = document.createElement('a');
                     link.href = '#';
 
                     let timestampElement = document.createElement('span');
-                    timestampElement.className = 'timestamp';  // Add class to the timestamp
+                    timestampElement.className = 'timestamp';
                     timestampElement.textContent = timestamp;
+
+                    if (!stopRequested) {
+                        timestampElement.className += ' active-timestamp';
+                        } else {
+                            timestampElement.className += ' stopped-timestamp';
+                        }
 
                     link.appendChild(timestampElement);
 
                     link.onclick = function() {
                         let [minutes, seconds] = timestamp.split(':');
-                        // Convert minutes and seconds to seconds, and subtract 2 seconds before setting the audio time
                         audioElement.currentTime = minutes * 60 + Number(seconds);
                         audioElement.play();
                         return false; 
@@ -78,8 +84,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     listItem.appendChild(transcriptElement);
 
                     let copyButton = document.createElement('button');
-                    copyButton.textContent = 'Copy';
                     copyButton.className = 'copy-button';
+
+                    let copyIcon = document.createElement('img');
+                    copyIcon.src = 'images/copy.svg';
+                    copyIcon.alt = 'Copy';
+
+                    copyButton.appendChild(copyIcon);
+
                     copyButton.addEventListener('click', function() {
                         let textarea = document.createElement('textarea');
                         textarea.textContent = `${timestamp}\n${transcript}`;
@@ -88,6 +100,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
                         document.execCommand('copy');
                         document.body.removeChild(textarea);
                     });
+
+                    copyButton.addEventListener('mousedown', function() {
+                        this.classList.add('copy-button-pressed');
+                     });
+
+                    copyButton.addEventListener('mouseup', function() {
+                        this.classList.remove('copy-button-pressed');
+                    });
+
 
                     listItem.appendChild(copyButton);
                     transcriptList.appendChild(listItem);
@@ -106,165 +127,165 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 voiceStatus.textContent = 'Undetected';
                 voiceStatus.style.color = 'red';
                 startTimer();
-            }, 2000);
-
-            if (transcripts.length === 1) {
-                // Enable the "Copy all" button when the first transcription is added
-                let copyAllButton = document.getElementById('copy-all');
-                copyAllButton.disabled = false;
-
-                // Set up the "Copy all" button
-                copyAllButton.addEventListener('click', function() {
-                    let allTranscriptions = transcripts.map(t => `${t.timestamp} - ${t.transcript}`).join('\n');
-                    navigator.clipboard.writeText(allTranscriptions)
-                        .then(function() {
-                            /* clipboard successfully set */
-                            console.log('Clipboard successfully set');
-                        }, function() {
-                            /* clipboard write failed */
-                            console.log('Clipboard write failed');
-                        });
-                });
-            }
+            }, 1600);
         }
 
         recognition.onend = function() {
             isListening = false;
-            if(!stopRequested && !isListening) {
-                recognition.start();
-            }
-        }
-
-        recognition.onerror = function(event) {
-            console.log('Error occurred in recognition: ' + event.error);
-            if (event.error === 'no-speech') {
-                recognition.start();
-            }
-        };
-    }
-
-    function startTimer() {
-        if (intervalId) {
-            clearInterval(intervalId);
-        }
-        timer = 0;
-        intervalId = setInterval(function() {
-            if (!VoiceRecognized) {
-                timer++;
-                timerDisplay.textContent = timer;
-            }
-        }, 1000);
-    }
-
-    function startGlobalTimer() {
-        globalTimer = 0;
-        globalIntervalId = setInterval(function() {
-            globalTimer++;
-            let minutes = Math.floor(globalTimer / 60);
-            let seconds = globalTimer % 60;
-            globalTimerDisplay.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-        }, 1000);
-    }
-
-    function stopGlobalTimer() {
-        clearInterval(globalIntervalId);
-        globalTimerDisplay.textContent = '0';
-    }
-
-    function startListening() {
-        stopRequested = false;
-        setupSpeechRecognition();
-        if(!isListening) {
+            if(!stopRequested && !isListening)
+        {
             recognition.start();
         }
-        startTimer();
-        startGlobalTimer();
-
-        navigator.mediaDevices.getUserMedia({ audio: true })
-            .then(function(stream) {
-                mediaStream = stream;
-                mediaRecorder = new MediaRecorder(stream);
-                mediaRecorder.start();
-                //startVisualizer(mediaStream);  // START VISUALIZER
-
-                mediaRecorder.ondataavailable = function(e) {
-                    recordedChunks.push(e.data);
-                };
-            });
     }
 
-    function stopListening() {
-        stopRequested = true;
-        recognition.stop();
-        clearTimeout(timeoutId);
-        clearInterval(intervalId);
-        stopGlobalTimer();
-        //stopVisualizer();  // STOP VISUALIZER
+    recognition.onerror = function(event) {
+        console.log('Error occurred in recognition: ' + event.error);
+        if (event.error === 'no-speech') {
+            recognition.start();
+        }
+    };
+}
 
-        if (mediaRecorder.stream) {
-            let tracks = mediaRecorder.stream.getTracks();
-            tracks.forEach(track => track.stop());
+function startTimer() {
+    if (intervalId) {
+        clearInterval(intervalId);
+    }
+    timer = 0;
+    intervalId = setInterval(function() {
+        if (!VoiceRecognized) {
+            timer++;
+            timerDisplay.textContent = timer;
+        }
+    }, 1000);
+}
+
+function startGlobalTimer() {
+    globalTimer = 0;
+    globalIntervalId = setInterval(function() {
+        globalTimer++;
+        let minutes = Math.floor(globalTimer / 60);
+        let seconds = globalTimer % 60;
+
+        let minutesStr = minutes < 10 ? minutes.toString() : minutes.toString().padStart(2, '0');
+        let secondsStr = seconds.toString().padStart(2, '0');
+
+        // Display the timer
+        if (minutes > 0) {
+            globalTimerDisplay.textContent = `${minutesStr}:${secondsStr}`;
+        } else {
+            globalTimerDisplay.textContent = `${secondsStr}`;
+        }
+    }, 1000);
+}
+
+function stopGlobalTimer() {
+    clearInterval(globalIntervalId);
+    globalTimerDisplay.textContent = '0';
+}
+
+function startListening() {
+    stopRequested = false;
+    setupSpeechRecognition();
+    if(!isListening) {
+        recognition.start();
+    }
+    startTimer();
+    startGlobalTimer();
+
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(function(stream) {
+            mediaStream = stream;
+            mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder.start();
+
+            mediaRecorder.ondataavailable = function(e) {
+                recordedChunks.push(e.data);
+            };
+        });
+}
+
+function stopListening() {
+    stopRequested = true;
+    recognition.stop();
+    clearTimeout(timeoutId);
+    clearInterval(intervalId);
+    stopGlobalTimer();
+
+    if (mediaRecorder.stream) {
+        let tracks = mediaRecorder.stream.getTracks();
+        tracks.forEach(track => track.stop());
+    }
+
+    mediaRecorder.stop();
+
+    mediaRecorder.onstop = function(e) {
+        const blob = new Blob(recordedChunks, {
+            'type' : 'audio/ogg; codecs=opus'
+        });
+        recordedChunks = [];
+        let audioURL = window.URL.createObjectURL(blob);
+
+        if (!audioElement) {
+            audioElement = new Audio();
+            audioElement.controls = true;
         }
 
-        mediaRecorder.stop();
+        audioElement.src = audioURL;
+        controlBar.appendChild(audioElement);
+        controlBar.appendChild(copyAllButton);
+    };
+}
 
-        mediaRecorder.onstop = function(e) {
-            const blob = new Blob(recordedChunks, {
-                'type' : 'audio/ogg; codecs=opus'
-            });
-            recordedChunks = [];
-            let audioURL = window.URL.createObjectURL(blob);
+const startButton = document.getElementById('start-button');
+const stopButton = document.getElementById('stop-button');
+const copyAllButton = document.getElementById('copy-all');
+const stopButtonContainer = document.getElementById('stop-button-container');
 
-            if (!audioElement) {
-                audioElement = new Audio();
-                audioElement.controls = true;
-            }
+copyAllButton.style.display = 'none';
+stopButton.style.display = 'none';
+startButton.addEventListener('click', function() {
+    // Start the listening and hide startButton
+    startListening();
+    this.style.display = 'none';
 
-            audioElement.src = audioURL;
+    // Show stopButton
+    stopButton.style.display = 'block';
 
-            let audioControlBar = document.getElementById('audio-control-bar');
-            audioControlBar.appendChild(audioElement);
+    // Hide copyAllButton when recording starts
+    copyAllButton.style.display = 'none';
 
-            // Create download button
-            let downloadButton = document.createElement('a');
-            downloadButton.innerHTML = "Download";
-            downloadButton.href = audioURL;
-            downloadButton.download = 'audio.ogg';
-            audioControlBar.appendChild(downloadButton);
-        };
-    }
+    // Add bottom border to controlBar
+    controlBar.style.borderBottom = "1px solid #2C2C2E";
+});
 
-    const startButton = document.getElementById('start-button');
-    const stopButton = document.getElementById('stop-button');
+stopButton.addEventListener('click', function() {
+    
+    stopListening();
+    const timestamps = document.querySelectorAll('.timestamp');
+    timestamps.forEach((timestamp) => {
+    timestamp.classList.remove('active-timestamp');
+    timestamp.classList.add('stopped-timestamp');
+});
+    stopButtonContainer.style.display = 'none';
+    
+    startButton.style.display = 'none';
 
-    // Make stopButton invisible by default
-    stopButton.style.display = 'none';
+    copyAllButton.style.display = 'block';
+});
 
-    startButton.addEventListener('click', function() {
-        // Start the listening and hide startButton
-        startListening();
-        this.style.display = 'none';
+copyAllButton.addEventListener('click', function() {
+    let textarea = document.createElement('textarea');
+    let allTranscriptions = transcripts.map(t => `${t.timestamp} - ${t.transcript}`).join('\n');
+    textarea.textContent = allTranscriptions;
+    
+    // Append textarea to audio control bar
+    controlBar.appendChild(textarea);
 
-        // Show stopButton
-        stopButton.style.display = 'block';
-    });
+    textarea.select();
+    document.execCommand('copy');
 
-    stopButton.addEventListener('click', function() {
-        // Stop the listening and hide stopButton
-        stopListening();
-        this.style.display = 'none';
+    // Remove textarea from audio control bar
+    controlBar.removeChild(textarea);
 
-        // Show startButton
-        startButton.style.display = 'block';
-    });
-
-    document.getElementById('copy-all').addEventListener('click', function() {
-        let allTranscriptions = transcripts.map(t => `${t.timestamp} - ${t.transcript}`).join('\n');
-        navigator.clipboard.writeText(allTranscriptions)
-            .then(function() {
-                console.log('Clipboard successfully set');
-            }, function() {
-                console.log('Clipboard write failed');
-            });
-    });
+});
 });
