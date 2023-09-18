c75cbeb127728a61672eeae3d9bee142b13311d8
erickgro <69096915+erickgro@users.noreply.github.com>
2023-09-18 02:54:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -45,29 +45,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
 
         recognition.onresult = function(event) {
-            console.log('Recognition result received');//SISEÑOR
-            
-            let currentTranscript = "";
+            console.log('Recognition result received');
             for (let i = event.resultIndex; i < event.results.length; i++) {
-                currentTranscript += event.results[i][0].transcript;
-                
                 if (VoiceRecognized === false) {
                     voiceRecognitionStart = globalTimer;
                     VoiceRecognized = true;
                 }
 
                 if (event.results[i].isFinal) {
-                    // We only consider final results to avoid repeated entries.
-                    
-                    let transcript = currentTranscript.trim();
-                    currentTranscript = ""; // Reset after using
+                    let transcript = event.results[i][0].transcript;
 
                     // Skip empty transcripts
-                    if (transcript === '') {
+                    if (transcript.trim() === '') {
                         continue;
                     }
 
-                    // Check for avoiding duplicates using the set
+                    // New check for avoiding duplicates using the set
                     if (lastTranscriptsSet.has(transcript)) {
                         console.log('Duplicate transcript detected. Skipping...');
                         continue;
